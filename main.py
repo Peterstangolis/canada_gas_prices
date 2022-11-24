57ef3fab060074e89ed7d4ab143a852dd3270458
Peter Stangolis <pstangolis@gmail.com>
2022-11-24 14:08:56 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import datetime
+
+
+
+
+
+def test_func():
+    print(datetime.datetime.today().date())
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    test_func()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
