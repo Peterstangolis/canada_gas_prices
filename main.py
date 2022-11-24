16620dd5f998c8d147ec9e0ef75ba03e56078204
Peter Stangolis <pstangolis@gmail.com>
2022-11-24 14:37:46 -0500

    setting up functions - folders

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 ## Importing my functions
-from my_functions.date_functions import add_suffix
+from my_functions.date_functions import add_suffix, todays_date, date_from_website
 
 
 ## Importing libraries
-import datetime
+import datetime as dt
+from datetime import timedelta
 import pandas as pd
 from bs4 import BeautifulSoup
 import streamlit as st
 import folium
 
 
+today, long_format_today, last_month = todays_date()
+tomorrow_long_format = add_suffix()
+date_from_gaswizard = date_from_website()
+
+tomorrow = today + timedelta(days=1)
+
+if date_from_gaswizard == tomorrow_long_format:
+    t = tomorrow
+else:
+    t = today
+
+print(date_from_gaswizard)
+print(tomorrow_long_format)
+print(t)
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Canadian Gas Prices", page_icon=":maple_leaf:")
 
 
 def test_func():
-    print(datetime.datetime.today().date())
+    print(dt.datetime.today().date())
 
 
 
@@ -24,5 +39,5 @@ def test_func():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     test_func()
-    add_suffix()
+    print(add_suffix())
 
--- my_functions/date_functions.py
+++ my_functions/date_functions.py
@@ -1,13 +1,14 @@
 
 ## A python file that contains a number of functions to return various dates required throughout the project
 
-from variables import url_gaswizard
+from variables import url_gaswizard, url_statscan
 
 from datetime import date, timedelta, datetime
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+import datetime as dt
 
 
 
@@ -36,8 +37,9 @@ def last_month_year_current():
     today, full_day, last_month = todays_date()
     return last_month.strftime("%b-%y")
 
+
 # 4. This function gets the date gaswizard.ca has been updated
-def date_from_gaswizard():
+def date_from_website():
     source = requests.get(url_gaswizard).text
     soup = BeautifulSoup(source, 'html.parser')
     t = soup.find('div', class_ = 'price-date').text
@@ -47,9 +49,19 @@ def date_from_gaswizard():
     week_day, day_num, month_name, long_year = website_date_list[0], ''.join(filter(str.isdigit, website_date_list[1])), \
                                                website_date_list[3], website_date_list[4]
     website_date_formatted_a = month_name + " " + day_num + ", " + long_year
-    website_date_formatted = datetime.datetime.strptime(website_date_formatted_a, "%B %d, %Y")
+    website_date_formatted = dt.datetime.strptime(website_date_formatted_a, "%B %d, %Y")
 
     return website_date
 
 
-add_suffix()
+# 5. Returns the date statscan site has been updated
+def stats_can_updated_date():
+    source = requests.get(url_statscan).text
+    soup = BeautifulSoup(source, 'html.parser')
+    # text = soup.find('div', class_= 'col-md-8 mrgn-btm-0 mrgn-lft-0 divLeft').text
+    para = soup.find('div', class_='col-md-8 mrgn-btm-0 mrgn-lft-0 divLeft')
+    for p in para:
+        if len(p.text) > 1:
+            text = p.text.strip()
+            stats_can_updated = (text[text.find('date:') + 5:text.find('date') + 37]).strip()
+            return stats_can_updated
